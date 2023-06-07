e6e217f30b7fd788b986fd42600a7ee39a723ee6
Yumomonteiro <yuriaco@hotmail.com>
2023-06-07 18:31:20 +0100

    header-site
    começando o desenvolvimento do header

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSearch() {
+    let aparecerPesquisa = document.getElementById('search-text');
+    if (aparecerPesquisa.style.display == "none") {
+        aparecerPesquisa.style.display = "block"
+    } else {
+        aparecerPesquisa.style.display = "none";
+    }
+}
+
